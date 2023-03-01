e0502304efca565f7149e1cbccf77e327e38947e
Anurag <anuragmarketing101@gmail.com>
2023-03-01 17:26:50 +0530

    factorial-func

---
+++ Recursion/main.js
@@ -0,0 +1,17 @@
+/*
+    1. Base case
+    2. Recursive call to itself 
+
+*/
+
+
+function factorial(n) {
+    if (n === 1 || n === 0) {
+        return 1
+    } else {
+        return n * factorial(n - 1)
+    }
+
+}
+
+console.log(factorial(4))
