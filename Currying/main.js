01294892d5f52db1fdb88b96ba6e2e77a8ef29d3
Anurag <anuragmarketing101@gmail.com>
2023-03-02 16:17:12 +0530

    curry()

--- Currying/main.js
+++ Currying/main.js
@@ -45,3 +45,37 @@ function add(a) {
 
 
 // Q4 => Currying vs Partial Application
+function sum_2(a) {
+    return function (b, c) {
+        return a + b + c
+    }
+}
+
+// Q5 => Manipulating DOM
+function updateText(id) {
+    return function (content) {
+        document.getElementById(id).textContent = content
+    }
+}
+const updateHeading = updateText("heading")
+// updateHeading("Anurag")
+
+
+// Q7 
+function curry(func) {
+    return function curriedFunc(...args) {
+        if (args.length >= func.length) {
+            return func(...args)
+        } else {
+            return function (...next) {
+                return curriedFunc(...args, ...next)
+            }
+        }
+    }
+}
+
+const sum_3 = (a, b, c) => a + b + c
+const totalSum = curry(sum_3)
+
+
+console.log(totalSum(1)(2)(3))
