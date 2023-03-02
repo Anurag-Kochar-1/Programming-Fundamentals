ea652506588a3780ba99dcc766978dbd84fd1012
Anurag <anuragmarketing101@gmail.com>
2023-03-02 14:54:15 +0530

    ForIn-and-ForOf

---
+++ Loops/ForInAndForOf.js
@@ -0,0 +1,17 @@
+const myName = 'anuragKochar'
+const myNums = [1, 3, 5, 7, 9, 11]
+
+// for (let n of myNums) {
+//     console.log(n)
+// }
+
+
+const myObject = {
+    name: "Anurag kochar",
+    age: 19,
+}
+
+
+for (let key in myObject) {
+    console.log(key)
+}
